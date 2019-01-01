4949bcdc7fbcd446ae269430f8a14c51dac47a4e
msa <malginsa@gmail.com>
2019-01-01 11:19:00 +0300

    added method launch() in IntervalUpdater

--- src/main/java/IntervalUpdater.java
+++ src/main/java/IntervalUpdater.java
@@ -9,18 +9,20 @@ public class IntervalUpdater {
     public static final int SCANNNING_INTERVAL = 9_000;
 
     public static void main(String[] args) throws Exception {
+        new IntervalUpdater().launch();
+    }
 
-        OnlineUsersSupplier onlineUsersSupplier = new OnlineUsersSupplier();
+    public void launch() throws IOException, InterruptedException {
+        OnlineUsersSupplier supplier = new OnlineUsersSupplier();
         DAO dao = new DAO();
         dao.startDbServerAndEstablishConnection();
         dao.createSchemasIfNeeded();
         UserInfoStorage userInfoStorage = new UserInfoStorage(
-                dao, onlineUsersSupplier.getSnapshotNow());
+                dao, supplier.getSnapshotNow());
 
         while (userInfoStorage != null) {
             Thread.sleep(SCANNNING_INTERVAL);
-            OnlineUsersSnapshot snapshot =
-                    onlineUsersSupplier.getSnapshotNow();
+            OnlineUsersSnapshot snapshot = supplier.getSnapshotNow();
             userInfoStorage.processNextSnapshot(snapshot);
             dao.storeSnapshot(snapshot);
             LOG.info(snapshot);
