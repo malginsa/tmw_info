abf19815acc3f95788cc61028a98fb0787d92e7f
msa <malginsa@gmail.com>
2019-01-04 07:43:57 +0300

    h2.sh added for console access to h2

---
+++ h2.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# This script will give you H2 database shell
+
+java -cp lib/default/h2*.jar org.h2.tools.Shell -url 'jdbc:h2:~/tmw_informer;AUTO_SERVER=TRUE' -user admin -password Yw3w79Ziw8DuhDZ
